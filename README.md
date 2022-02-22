50fc27d94529fc9c2fe2f9dda6bb75c6c8c6ebd2
abdulrahman <75491649+papilo-cloud@users.noreply.github.com>
2022-02-22 13:59:01 -0800

    Add files via upload
    This is a solution to the [GitHub user search app challenge on Frontend Mentor]

---
+++ README.md
@@ -0,0 +1,76 @@
+# Frontend Mentor - GitHub user search app solution
+
+This is a solution to the [GitHub user search app challenge on Frontend Mentor]. Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- See the size of the elements adjust based on their device's screen size
+- See hover state for all interactive elements on the page
+- Searxh for GitHub by their username
+- See relevant user information based on their search
+- Switch between light and dark mode
+
+### Screenshot
+
+![](Screen.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- Javascript
+- CSS For styles
+
+
+### What I learned
+
+I Learned about APIs, how to fetch them and convert to a javascript object
+using the javascript fetch API method, GitHub API to be precise.
+
+```js
+async function nyFile(){
+  let x = await fetch()
+  let y = await x.json()
+}
+```
+
+### Continued development
+
+To learn more about APIs
+
+## Author
+
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/papilo-cloud)
+- Twitter - [@yourusername](https://www.twitter.com/@Abdulra75754192)
+
+
+
+## Acknowledgments
+
+Brad Traversy, I'm a big fan
+W3School.
---
+++ script.js
@@ -0,0 +1,111 @@
+
+//Declearing all variabes..
+var mode = document.getElementById('mode')
+var txt = document.getElementById('txt')
+var img = document.getElementById('img')
+var nme = document.getElementById('name')
+var username = document.getElementById('username')
+var joined = document.getElementById('joined')
+var demo = document.getElementById('demo')
+var spn1 = document.getElementById('spn1')
+var spn2 = document.getElementById('spn2')
+var spn3 = document.getElementById('spn3')
+var id1 = document.getElementById('id1')
+var id2 = document.getElementById('id2')
+var id3 = document.getElementById('id3')
+var id4 = document.getElementById('id4')
+var cont = document.getElementsByClassName('container')[0]
+
+
+// For changing the mode
+mode.onclick = function () {
+    bdy.classList.toggle('dark')
+    cont.classList.toggle('dark')
+}
+
+
+// //## Fetching the API From GitHub...
+// getFile('https://api.github.com/users/bradtraversy')
+
+async function getFile(file) {
+    let x = await fetch(file);
+    let y = await x.json()
+    console.log(y)
+
+    if (y == null) {
+        this.innerHTML = 'Not available'
+    }
+
+    var mths = ['Jan','Feb','March','April','May','June','July','August','Sept','Oct','Nov','Dec']
+
+    let strng = y.created_at
+    let len = strng.substr(0,10)
+
+    let rep = len.replace(/-/g,'')
+    let ars = rep.substring(4,6)
+    let ars1 = len.split('-').reverse()
+
+    let date = ars1[0]+' '+mths[ars - 1]+' '+ars1[2]
+    joined.innerHTML = date
+
+
+    console.log(len)
+    nme.innerHTML = y.name
+    img.src = y.avatar_url
+    username.innerHTML = y.login
+   
+    spn1.innerHTML = y.public_repos
+    spn2.innerHTML = y.followers
+    spn3.innerHTML = y.following
+    demo.innerHTML = y.bio
+    
+    if (y.twitter_username == null ) {  
+        id3.innerHTML = 'not avaiable'  
+        id3.classList.add('avail') 
+    }
+     else{
+        id3.innerHTML = y.twitter_username
+     }
+
+     if (y.location == null ) {  
+        id1.innerHTML = 'not avaiable'
+        id1.classList.add('avail') 
+    }
+     else{
+        id1.innerHTML = y.location
+     }
+
+     if (y.blog == null || y.blog == '') {  
+        id2.innerHTML = 'not avaiable'  
+        id2.classList.add('avail') 
+    }
+     else{
+        id2.innerHTML = y.blog
+     }
+
+     if (y.company == null ) {  
+        id4.innerHTML = 'not avaiable'  
+        id4.classList.add('avail') 
+    }
+     else{
+        id4.innerHTML = y.company
+     }
+    //     id1.innerHTML = y.location
+    //     id2.innerHTML = y.blog
+    //     
+    //     id4.innerHTML = y.company
+    // }
+    
+    
+}
+
+
+//## Button function for displaying the search result..
+function myBtn() {
+    
+getFile('https://api.github.com/users/'+txt.value+'?client_id=8914d05319298ca33344&client_secret=707bb9408d5622046e2a8001ac8c26bc3782f725')
+}
+
+// 
+// 
+
